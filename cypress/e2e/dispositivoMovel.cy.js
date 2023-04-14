6e21b8e497cbabb71a74c5b5c0ee3c5f05564b84
Iara Tassi <iara.tassinh@gmail.com>
2023-04-14 10:11:01 -0300

    alterando código disposivo móvel

--- cypress/e2e/dispositivoMovel.cy.js
+++ cypress/e2e/dispositivoMovel.cy.js
@@ -1,4 +1,15 @@
+const sizes = ['iphone-6', 'ipad-2', [1024, 768]]
+
+
 describe('Testando dispositivos móveis', ()=> {
+    sizes.forEach((size) => {
+        it(`Should display logo on ${size} screen`, () => {
+            if (Cypress._.isArray(size)) {
+              cy.viewport(size[0], size[1])
+            } else {
+              cy.viewport(size)
+            }
+
 it('Deve existir um botão menu burguer', ()=> {
             cy.visit('/')
             cy.getByData('botao-login').click()
@@ -15,8 +26,12 @@ it('Deve existir um botão menu burguer', ()=> {
 
         })    
     })
+})
+
+})
     
 
 
 // para rodar este teste é necessário rodar comando 
 // npx cypress open --config vieportWidth=375, viewportHeight=667
+// Este método a cima aprendi no curso, mas não é ideal pois quando for rodar novos testes eles quebram se não fechar fechar e abrir de novo com comando npx cypress open, então olhando a documentação usei uma const e um it em que chamo a const, método correto.
